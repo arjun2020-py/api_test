ffb1d4f2e7bbbdb88d6b54f2b6271fcc652e499c
arjun2020-py <arjunkoodathil@gmail.com>
2024-03-18 15:48:00 +0530

    make some updation in seatiled screen

--- lib/screen/screen3/show/view/screen_detailed.dart
+++ lib/screen/screen3/show/view/screen_detailed.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart' as material;
 
+import '../compoents/screen_custom_elev.dart';
 import '../model/show_model.dart';
-
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:intl/intl.dart';
 class ScreenDetiledShow extends material.StatelessWidget {
   ScreenDetiledShow({super.key, required this.shows});
   ShowModel shows;
@@ -11,13 +13,50 @@ class ScreenDetiledShow extends material.StatelessWidget {
     return material.Scaffold(
       backgroundColor: material.Colors.black,
       appBar: material.AppBar(
-        leading: material.IconButton(
-          onPressed: () => material.Navigator.of(context).pop(),
-          icon: material.Icon(
-            material.Icons.arrow_back,
-            color: material.Colors.red,
+        leading: material.Container(
+          decoration: material.BoxDecoration(
+              shape: material.BoxShape.circle,
+              color: material.Color(0xffd9e3f5)),
+          child: material.IconButton(
+            onPressed: () => material.Navigator.of(context).pop(),
+            icon: material.Icon(
+              material.Icons.arrow_back_ios_new_rounded,
+              color: material.Colors.red,
+            ),
           ),
         ),
+        actions: [
+          material.Container(
+            decoration: material.BoxDecoration(
+                shape: material.BoxShape.circle,
+                color: material.Color(0xffd9e3f5)),
+            child: material.IconButton(
+              onPressed: () => material.Navigator.of(context).pop(),
+              icon: material.Icon(
+                material.Icons.bookmark,
+                color: material.Colors.red,
+              ),
+            ),
+          ),
+          material.SizedBox(
+            width: 10,
+          ),
+          material.Container(
+            decoration: material.BoxDecoration(
+                shape: material.BoxShape.circle,
+                color: material.Color(0xffd9e3f5)),
+            child: material.IconButton(
+              onPressed: () => material.Navigator.of(context).pop(),
+              icon: material.Icon(
+                material.Icons.share,
+                color: material.Colors.red,
+              ),
+            ),
+          ),
+           material.SizedBox(
+            width: 10,
+          ),
+        ],
         backgroundColor: material.Colors.transparent,
       ),
       body: material.SingleChildScrollView(
@@ -28,29 +67,45 @@ class ScreenDetiledShow extends material.StatelessWidget {
                 shows.image.original,
                 width: 200,
               ),
-              material.SizedBox(
-                width: material.MediaQuery.sizeOf(context).width * 0.7,
-                child: material.ElevatedButton.icon(
-                  icon: material.Icon(material.Icons.play_arrow),
-                  onPressed: () {},
-                  label: material.Text('Play Now'),
-                ),
-              ),
-              material.Text(
-                shows.language.name,
-                style: material.TextStyle(
-                    color: material.Colors.white, fontSize: 15),
-              ),
               material.Text(
                 shows.name,
                 style: material.TextStyle(
                     color: material.Colors.white, fontSize: 30),
               ),
               material.Text(
-                shows.summary,
+                shows.language.name,
                 style: material.TextStyle(
-                    color: material.Colors.white, fontSize: 14),
-              )
+                    color: material.Colors.white, fontSize: 15),
+              ),
+            
+              
+              material.SizedBox(
+                height: 10,
+              ),
+              material.Row(
+                mainAxisAlignment: material.MainAxisAlignment.spaceAround,
+                children: [
+                  ShowElvButton(
+                    text: 'Play ',
+                    bgColor: material.Colors.red,
+                    width: 0.4,
+                    icons: material.Icons.play_arrow,
+                  ),
+                  ShowElvButton(
+                    text: 'Download',
+                    bgColor: material.Colors.white,
+                    width: 0.4,
+                    icons: material.Icons.download,
+                  )
+                ],
+              ),
+              material.SizedBox(
+                height: 10,
+              ),
+              HtmlWidget(
+                shows.summary,
+                textStyle: material.TextStyle(color: material.Colors.white),
+              ),
             ],
           ),
         ),
